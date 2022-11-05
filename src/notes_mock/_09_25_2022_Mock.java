491eb4a0f41525804f5eb3c997cd535045143651
Ashraf Musleh <muslehashraf@gmail.com>
2022-11-05 12:11:52 -0500

    ..

---
+++ src/notes_mock/_09_25_2022_Mock.java
@@ -0,0 +1,43 @@
+package notes_mock;
+
+public class _09_25_2022_Mock {
+    /*
+            MOCK INTERVIEW 1
+            Duration: 30 minutes
+            Soft-skill knowledge questions: 5 points - 2 questions and 5 minutes
+            Java knowledge questions: 		5 points - 5 questions and 5 minutes
+            Java whiteboard questions: 		10 points - 20 minutes
+            NOTES FOR BOTH CAMPUS AND ONLINE STUDENTS
+            -YOU ARE NOT ALLOWED TO USE INTELLIJ IDEA
+            -make sure you are on time - DO NOT BE LATE
+            -NO NOTES or READING ALLOWED
+            -DO NOT SHARE QUESTIONS AFTER YOUR MOCK IS DONE
+            -online
+                -make sure you have sublime ready before you join the interview
+                -make sure your audio and video works fine
+                -make sure you can share screen with zoom meeting
+            -campus
+                -3rd and 4th floor, you can see your interviewer's name on the private room door
+                -you don't need to have your laptop, we will be using whiteboards
+                Soft-skills focus:
+                    SDLC
+                    Scrum
+                    Agile
+                    Ceremonies
+                    Scrum-sprint artifacts
+                Java focus:
+                    Data types
+                    Conditional statements
+                    Casting
+                    Whiteboard
+            SCHEDULE: Saturday, you will learn your time and interviewer
+            10:00 to 12:30
+            AFTER THE MOCK - Same day results
+        AGREEMENT for the 6 months:
+            -DocuSign - will be sent on Monday/Tuesday and DUE DATE: Wednesday
+            -Payment options: Credit/Debit card
+            -First payment: 5th of October
+        AFTER MOCK
+            -Mentoring sessions with groups
+     */
+}
---
+++ src/notes_mock/_11_06_2022_Mock.java
@@ -0,0 +1,29 @@
+package notes_mock;
+
+public class _11_06_2022_Mock {
+    /*
+            Mock:
+        SUNDAY:
+        Smart phones will handle it :)
+        10 AM to 1 PM
+        45 minutes
+        Java
+            -Array-ArrayList-LinkedList-Vector
+            -Primitives
+            -String
+            -Casting
+            -Loops
+        Soft skills
+            -GIT commands - merge conflict resolution
+            -CLI Linux commands
+            -User story - test case - test scenario - edge cases - boundary - positive and negatives
+        Whiteboard
+            -String methods, loops, array - arraylist
+            -Codingbat (String1, Array1, Warmup1), reverse, fibonacci, counting elements, find max, find min, second max, second min, sum, product
+        QUESTIONS:
+        4 soft skills
+        2 Java questions
+        3 whiteboards
+        Total: 9 Questions
+     */
+}
