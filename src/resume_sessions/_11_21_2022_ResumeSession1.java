9653438c7a62ae53a99d0ca51e0270f8ac0f6eb3
Ashraf Musleh <muslehashraf@gmail.com>
2022-11-21 19:59:27 -0600

    ..

---
+++ src/resume_sessions/_11_21_2022_ResumeSession1.java
@@ -0,0 +1,60 @@
+package resume_sessions;
+
+public class _11_21_2022_ResumeSession1 {
+    /*
+            11/21/2022 - Resume session 1
+        Technical Skills
+        Programming language: Java, SQL, HTML, CSS
+        Version control: GIT, GitHub, GitLab, BitBucket
+        Project management tool: JIRA, Internal tools
+        Methodologies: Agile, Waterfall, SCRUM, SAFe
+        Utilities: Slack, Miro, Microsoft Office, etc.
+        IDE: IntelliJ IDEA, Eclipse, VSCode, SQL Developer, Postman
+        Automation tools: Selenium WebDriver, Appium Cucumber, TestNG, JUnit, RestAPI - RestAssured, JDBC
+        Testing types: Smoke, Regression, E2E (End-to-end), Integration, System, Performance, Security
+        Frameworks: TDD, BDD, DDD, Hybrid, Keyword Driven
+        Personal Information
+            Fullname
+            Email
+            Phone
+            linkedin-url
+            Might have
+            github-url
+            City - State
+            SHOULD NOT be there
+            Full address
+            Date of birth
+            Picture
+        Professional Summary
+            -Years of experience
+            -Main knowledge - methodologies etc.
+            -Testing types
+            -Strengths
+            -Main tools
+        Professional Experience 4-6 years 2-3 companies
+            Company 1 | start date - current
+            - Deeply involved in UI testing using Java and Selenium
+            -
+            -
+            -
+            Company 2 | start date - end date
+            -
+            -
+            -
+            -
+            Company 3 | start date - end date
+            -
+            -
+            -
+            -
+        Education
+        2018 - 2022 ->
+        2 companies
+            Recent one -> Automation - 100 percent
+            Old one -> manual + automation
+        3 Companies
+            Recent one -> Automation - 100 percent
+            Middle -> manual + automation
+            Old one -> manual + involved in automation
+     */
+}
