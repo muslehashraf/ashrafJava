da8a334d21a975fd5ea1155ea3bcfd924f955bfb
Ashraf Musleh <muslehashraf@gmail.com>
2022-12-11 14:06:34 -0600

    ..

--- src/first_package/HelloWorld.java
+++ src/first_package/HelloWorld.java
@@ -2,9 +2,27 @@ package first_package;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Ashraf");
-        System.out.println("Musleh");
-    }
+        //Single line Comments
 
+        /*
+        Multi-line comments
+        The below lines are the code statements
+        It is ideal to end each code statement with a semicolon
+        EACH STATEMENT MUST BE FOLLOWED WITH A SEMICOLON
+         */
+
+        System.out.println("Hello World!");
+        System.out.println("I started to learn JAVA");
+        System.out.println("I liked JAVA, and My name is John");
+
+        /*
+        TASK
+        Print your name
+        Print your last name
+         */
+
+        System.out.println("John");
+        System.out.println("Doe");
+
+    }
 }
