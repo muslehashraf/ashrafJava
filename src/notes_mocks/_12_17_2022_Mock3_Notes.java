6319377418d6ad7fd7e3a74dcff441ad6e279afc
Ashraf Musleh <muslehashraf@gmail.com>
2022-12-17 16:02:39 -0600

    ..:

---
+++ src/notes_mocks/_12_17_2022_Mock3_Notes.java
@@ -0,0 +1,43 @@
+package notes_mocks;
+
+public class _12_17_2022_Mock3_Notes {
+
+    /*
+                    Mock-3
+        Soft-skills knowledge -
+            Testing types - GIT
+            merge vs rebase
+            branching commands
+            Bug Life Cycle - Reports
+            SAFe
+            SMART
+        Java knowledge
+            Exceptions
+            Collections
+            OOP
+                Class members
+                Garbage collection
+                Access modifiers
+                OOP concepts
+                    What is it?
+                    How to achieve it?
+                    What is the main goal / advantages?
+            Casting
+        Java Whiteboard
+            Collections - loops - conditions - pseudo code
+            Map - Set
+        Friday 10 AM to 1 PM ->
+        Lunch together
+
+
+        Monday 6 PM meeting to discuss Mock-3 details
+        Tuesday - Wednesday - Thursday - OFF
+        Document TO DO -> January 2nd
+        Map-1
+        Map-2
+        AP-1
+        Homework-13
+        Project10
+        Selenium Installation
+     */
+}
